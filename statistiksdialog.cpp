2abbb634b4ecc25ea4bd41ac80fa404533618395
Galaxyqasar <kunze.georg44@gmail.com>
2017-07-30 13:19:49 +0200

    Add files via upload

--- statistiksdialog.cpp
+++ statistiksdialog.cpp
@@ -24,22 +24,23 @@ void StatistiksDialog::inittable()
     headers << "Score"<<"gewonnen/verloren"<<"falsche/richtige Buchstaben";
     stats = readstats();
     QString tmp;
-    ui->tableWidget->setSortingEnabled(false);
-    ui->tableWidget->setRowCount(3);
     ui->tableWidget->setColumnCount(3);
     ui->tableWidget->setHorizontalHeaderLabels(headers);
-//    ui->tableWidget->setColumnWidth(0, ui->tableWidget->horizontalHeader()->length()/2);
-//    ui->tableWidget->setColumnWidth(1, ui->tableWidget->horizontalHeader()->length()/2);
-//    ui->tableWidget->setColumnWidth(2, ui->tableWidget->horizontalHeader()->length()/2);
+    ui->tableWidget->setColumnWidth(0, ui->tableWidget->columnWidth(0)*1);
+    ui->tableWidget->setColumnWidth(1, ui->tableWidget->columnWidth(1)*2);
+    ui->tableWidget->setColumnWidth(2, ui->tableWidget->columnWidth(2)*2);
     ui->tableWidget->horizontalHeader()->sortIndicatorOrder();
+    for(int i=stats.size();i>=0;i=i-2)
+        stats.removeAt(i-1);
+    ui->tableWidget->setRowCount(stats.size());
     for(int i=0;i<stats.size();i++)
     {
         tmp = stats.at(i);
-        ui->tableWidget->setItem(0, 0, new QTableWidgetItem(QString(strtok(tmp.toLatin1().data(), "-:-"))));
+        ui->tableWidget->setItem(i, 1, new QTableWidgetItem(QString(strtok(tmp.toLatin1().data(), "-:-"))));
         strtok(NULL, ":");
-        ui->tableWidget->setItem(0, 1, new QTableWidgetItem(QString(strtok(NULL, "-:-"))));
+        ui->tableWidget->setItem(i, 2, new QTableWidgetItem(QString(strtok(NULL, "-:-"))));
         strtok(NULL, ":");
-        ui->tableWidget->setItem(0, 1, new QTableWidgetItem(QString(strtok(NULL, ";"))));
+        ui->tableWidget->setItem(i, 0, new QTableWidgetItem(QString(strtok(NULL, ";"))));
     }
 }
 
